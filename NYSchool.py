a5fa646f6160c465eb4db81198ac14fe182f74b0
Nirmal Prakash <nirmalp2@illinois.edu>
2018-06-23 22:59:32 -0500

    Initial Work

---
+++ NYSchool.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+
+files = ["ap.csv", "class.csv", "demographics.csv", "grad.csv", "math.csv", "sat.csv", "school.csv"]
+
+data = {}
+for f in files:
+    d = pd.read_csv(f)
+    data[f.replace(".csv","")] = d
+
+data["school"]["DBN"] = data["school"]["dbn"]
+
+for k,v in data.items():
+    print("\n" + k + "\n")
+    print(v.head())
+
