30c4b0b61ea9979e7893361968d6fba49a03938c
Nirmal Prakash <nirmalp2@illinois.edu>
2018-06-25 00:59:57 -0500

    DBN

--- NYSchool.py
+++ NYSchool.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import numpy as np
 
+# File names
 files = ["ap.csv", "class.csv", "demographics.csv", "grad.csv", "math.csv", "sat.csv", "school.csv"]
 
+# Importing data
 data = {}
 for f in files:
     d = pd.read_csv("Data/" + f)
     data[f.replace(".csv","")] = d
 
+# Adding DBN
 data["school"]["DBN"] = data["school"]["dbn"]
+data["class"]["DBN"] = data["class"].apply(lambda x: "{0:02d}{1}".format(x["CSD"],x["SCHOOL CODE"]), axis=1)
 
-for k,v in data.items():
-    print("\n" + k + "\n")
-    print(v.head())
+# Code to check data
+#for k,v in data.items():
+#    print("\n" + k + "\n")
+#    print(v.head())
+
+
 
